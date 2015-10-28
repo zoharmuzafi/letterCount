f55479b722683f2d58a805dbcb1782836b3021b5
e3 <eerwitt@gmail.com>
2015-10-28 14:27:01 -0700

    Initial commit of test code.

--- letterCount.js
+++ letterCount.js
@@ -1,6 +1,13 @@
+/*
+ * Write a function that counts the number of times each letter occurs in a given string. It should return an object containing the count for each letter.
+ * @param word {string} Word to count letters in.
+ * @returns characterCount {Object} Count of each character in the word.
+ * @example
+ * // returns {"a": 1, "p": 2, "l": 1, "e": 1}
+ * letterCount("apple");
+ * @todo Implement this function then remove this todo.
+ **/
 function letterCount(word) {
-
+  var characterCount = {};
+  return characterCount;
 }
-
-
-module.exports = letterCount;
